2b2f3cfbe23fece25d439fce072a8f0ae374844b
valdirei <valdireifm@gmail.com>
2020-08-30 11:31:27 -0300

    Instanciação da categoria

--- src/main/java/com/direi/cursomc/CursomcApplication.java
+++ src/main/java/com/direi/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.direi.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.direi.cursomc.domain.Categoria;
+import com.direi.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Construção");
+		categoriaRepository.saveAll(Arrays.asList(cat,cat2,cat3));
+		
 	}
 
 }
