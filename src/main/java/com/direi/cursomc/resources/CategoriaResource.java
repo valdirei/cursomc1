2525023d7967f825969974bc6b1bd383ecc81dd7
valdirei <valdireifm@gmail.com>
2020-08-30 11:08:34 -0300

    Camada de serviço refatoração do end point categoria

--- src/main/java/com/direi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/direi/cursomc/resources/CategoriaResource.java
@@ -3,24 +3,27 @@ package com.direi.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.direi.cursomc.domain.Categoria;
+import com.direi.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-
-	@GetMapping
-	public List<Categoria> lsitar() {
-		Categoria cat = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		List<Categoria> list = new ArrayList<>();
-		list.add(cat);
-		list.add(cat2);
+	
+	@Autowired
+	private CategoriaService service;
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		return list;
+		Categoria cat = service.buscar(id);
+		
+		return ResponseEntity.ok(cat) ;
 	}
 }
