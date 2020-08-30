867c54938da757383121905734988cc201f7f4aa
valdirei <valdireifm@gmail.com>
2020-08-30 09:10:53 -0300

    Classe categoria criada

--- src/main/java/com/direi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/direi/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.direi.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.direi.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String lsitar() {
-		return "Opa projeto meu !!!!";
+	public List<Categoria> lsitar() {
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat);
+		list.add(cat2);
+		
+		return list;
 	}
 }
