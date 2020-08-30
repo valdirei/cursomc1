0cab7311a227a3e5acabcebd73c6f800da67ba54
valdirei <valdireifm@gmail.com>
2020-08-30 00:12:00 -0300

    Criando classe categoria

---
+++ src/main/java/com/direi/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.direi.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@GetMapping
+	public String lsitar() {
+		return "Opa projeto meu !!!!";
+	}
+}
