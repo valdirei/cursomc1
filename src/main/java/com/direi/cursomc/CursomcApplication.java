3a249d901f5bf1b2054a61668c6d587a9f7cf5dc
valdirei <valdireifm@gmail.com>
2020-08-31 20:13:11 -0300

    Estado e Cidade

--- src/main/java/com/direi/cursomc/CursomcApplication.java
+++ src/main/java/com/direi/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.direi.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.direi.cursomc.domain.Categoria;
+import com.direi.cursomc.domain.Cidade;
+import com.direi.cursomc.domain.Estado;
 import com.direi.cursomc.domain.Produto;
 import com.direi.cursomc.repositories.CategoriaRepository;
+import com.direi.cursomc.repositories.CidadeRepository;
+import com.direi.cursomc.repositories.EstadoRepository;
 import com.direi.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +25,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,6 +56,23 @@ public class CursomcApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+	
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		
+		
 		
 	}
 
