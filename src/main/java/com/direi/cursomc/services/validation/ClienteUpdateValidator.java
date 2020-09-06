0cbde7488e63aaf00233232df10012f015420618
valdirei <valdireifm@gmail.com>
2020-09-06 12:08:57 -0300

    Validação customizada para não repeticao de email

--- src/main/java/com/direi/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/direi/cursomc/services/validation/ClienteUpdateValidator.java
@@ -2,11 +2,14 @@ package com.direi.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.direi.cursomc.domain.Cliente;
 import com.direi.cursomc.domain.enums.TipoCliente;
@@ -15,10 +18,13 @@ import com.direi.cursomc.repositories.ClienteRepository;
 import com.direi.cursomc.resources.exception.FieldMessage;
 import com.direi.cursomc.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private HttpServletRequest request;
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,16 +33,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
-			list.add(new FieldMessage("cpfOuCnpj","Cpf Inválido"));
-		}
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE); 
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
-			list.add(new FieldMessage("cpfOuCnpj","Cnpj Inválido"));
-		}
-		
+		Integer uriId = Integer.parseInt(map.get("id"));
 		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
-		if(cliente != null) {
+		if(cliente != null && !uriId.equals(cliente.getId())) {
 			list.add(new FieldMessage("email", "Email já cadastrado no banco de dados."));
 		}
 		// inclua os testes aqui, inserindo erros na lista
