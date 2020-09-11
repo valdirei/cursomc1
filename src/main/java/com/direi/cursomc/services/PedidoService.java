05693caa170629f96a16c9de76e5c9bb215f30cd
valdirei <valdireifm@gmail.com>
2020-09-11 14:42:35 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/direi/cursomc/services/PedidoService.java
+++ src/main/java/com/direi/cursomc/services/PedidoService.java
@@ -4,9 +4,14 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.direi.cursomc.domain.Categoria;
+import com.direi.cursomc.domain.Cliente;
 import com.direi.cursomc.domain.ItemPedido;
 import com.direi.cursomc.domain.PagamentoComBoleto;
 import com.direi.cursomc.domain.Pedido;
@@ -14,6 +19,8 @@ import com.direi.cursomc.domain.enums.EstadoPagamento;
 import com.direi.cursomc.repositories.ItemPedidoRepository;
 import com.direi.cursomc.repositories.PagamentoRepository;
 import com.direi.cursomc.repositories.PedidoRepository;
+import com.direi.cursomc.security.UserSS;
+import com.direi.cursomc.services.exception.AuthorizationException;
 import com.direi.cursomc.services.exception.ObjectNotFoundException;
 
 
@@ -71,5 +78,15 @@ public class PedidoService {
 		//System.out.println(obj);
 		
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso neado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente,pageRequest);
 	}
 }
