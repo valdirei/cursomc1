70540b2f970c96c0c6c2c4c29f739a91e255aa45
valdirei <valdireifm@gmail.com>
2020-09-11 12:23:55 -0300

    Implementando autorização

--- src/main/java/com/direi/cursomc/config/SecurityConfig.java
+++ src/main/java/com/direi/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.direi.cursomc.security.JWTAuthenticationFilter;
+import com.direi.cursomc.security.JWTAuthorizationFilter;
 import com.direi.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.antMatchers(PUBLIC_MATCHES).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
